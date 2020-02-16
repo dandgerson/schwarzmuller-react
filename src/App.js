d417a270c651a1f7bd6b6e32df193fb27a182295
Dmitry G. Anderson <dandgerson@gmail.com>
2020-02-16 21:46:28 +0300

    39 understanding using Props&State

--- src/App.js
+++ src/App.js
@@ -5,16 +5,28 @@ import './App.css';
 import Person from './Person'
 
 class App extends Component {
+  state = [
+    { name: 'Dmitry', age: 32 },
+    { name: 'Max', age: 24 },
+    { name: 'Stepan', age: 32 },
+  ]
   render() {
     return (
       <div className="App">
-        <h1>Hi, I'am react App!</h1>
+        <h1>Hi, I'am react App</h1> 
+        <button>Switch persons</button>
         <Person
-          name='Dmitry'
-          age='32'
-        >
-          {/* <p>Hello Dude!</p> */}
-        </Person>
+          name={this.state[0].name}
+          age={this.state[0].age}
+        />
+        <Person
+          name={this.state[1].name}
+          age={this.state[1].age}
+        />
+        <Person
+          name={this.state[2].name}
+          age={this.state[2].age}
+        />
       </div>
     );
   }
