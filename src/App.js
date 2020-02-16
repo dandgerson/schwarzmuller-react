d8956dc154461ec21ac6b4af37fe7c7df1e4d953
Dmitry G. Anderson <dandgerson@gmail.com>
2020-02-16 22:10:25 +0300

    42 manipulating the State

--- src/App.js
+++ src/App.js
@@ -5,27 +5,38 @@ import './App.css';
 import Person from './Person'
 
 class App extends Component {
-  state = [
-    { name: 'Dmitry', age: 32 },
-    { name: 'Max', age: 24 },
-    { name: 'Stepan', age: 32 },
-  ]
+  state = {
+    persons: [
+      { name: 'Dmitry', age: 32 },
+      { name: 'Max', age: 24 },
+      { name: 'Stepan', age: 32 },
+    ],
+  }
+  handleSwitchPerson = () => {
+    this.setState({
+      persons: [
+        { name: 'Dmitry G. Anderson', age: 32 },
+        { name: 'Maxim P. Pavlov', age: 24 },
+        { name: 'Stepan V. Motovilov', age: 32 },
+      ],
+    })
+  }
   render() {
     return (
       <div className="App">
-        <h1>Hi, I'am react App</h1> 
-        <button>Switch persons</button>
+        <h1>Hi, I'am react App</h1>
+        <button onClick={this.handleSwitchPerson}>Switch persons</button>
         <Person
-          name={this.state[0].name}
-          age={this.state[0].age}
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
         />
         <Person
-          name={this.state[1].name}
-          age={this.state[1].age}
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
         />
         <Person
-          name={this.state[2].name}
-          age={this.state[2].age}
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
         />
       </div>
     );
