0bb9be9f97604325f0b4f62ad293d1d867b5d758
Dmitry G. Anderson <dandgerson@gmail.com>
2020-02-24 04:43:38 +0300

    46 Passing method reference between components

--- src/App.js
+++ src/App.js
@@ -1,46 +1,45 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 import './App.css';
 
 import Person from './Person'
 
-class App extends Component {
-  state = {
+const App = (props) => {
+  const [state, setState] = useState({
     persons: [
       { name: 'Dmitry', age: 32 },
       { name: 'Max', age: 24 },
       { name: 'Stepan', age: 32 },
     ],
-  }
-  handleSwitchPerson = () => {
-    this.setState({
+  })
+  const handleSwitchPerson = (newName) => {
+    setState({
       persons: [
-        { name: 'Dmitry G. Anderson', age: 32 },
+        { name: newName, age: 32 },
         { name: 'Maxim P. Pavlov', age: 24 },
         { name: 'Stepan V. Motovilov', age: 32 },
       ],
     })
   }
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi, I'am react App</h1>
-        <button onClick={this.handleSwitchPerson}>Switch persons</button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-        />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-        />
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}
-        />
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <h1>Hi, I'am react App</h1>
+      <button onClick={() => handleSwitchPerson('Dmitry G. Anderson')}>Switch persons</button>
+      <Person
+        name={state.persons[0].name}
+        age={state.persons[0].age}
+      />
+      <Person
+        name={state.persons[1].name}
+        age={state.persons[1].age}
+        click={handleSwitchPerson}
+      />
+      <Person
+        name={state.persons[2].name}
+        age={state.persons[2].age}
+      />
+    </div>
+  )
 }
 
 export default App;
--- src/Person/index.js
+++ src/Person/index.js
@@ -1,10 +1,16 @@
 import React from 'react'
 
-const Person = (props) => {
+const Person = ({ click = () => {}, ...props}) => {
+  const style = {
+    cursor: 'pointer',
+  }
   return (
     <div>
       <p>I'm a Person!</p>
-      <p>My name is {props.name} and I am {props.age} years old.</p>
+      <p
+        onClick={() => click('Dmitry')}
+        style={style}
+      >My name is {props.name} and I am {props.age} years old.</p>
       {props.children && props.children}
     </div>
   )
