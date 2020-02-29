78a7c4c5c58fbdddb5f6489e31bc676b6d6d14fb
Dmitry G. Anderson <dandgerson@gmail.com>
2020-02-29 19:19:46 +0300

    assignment-2 step 2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 
 import './App.css';
 
+import LengthChecker from './LengthChecker'
+
 const App = (props) => {
   const [str, setStr] = useState('')
 
@@ -14,6 +16,8 @@ const App = (props) => {
       <h1>Hi, I'am react App</h1>
       <input type="text" onChange={handleChangeStr} value={str} />
       <p>{str.length}</p>
+
+      <LengthChecker length={str.length} />
     </div>
   )
 }
---
+++ src/LengthChecker/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LengthChecker = ({ length }) => {
+  return (
+    <p>{length} is valid</p>
+  )
+}
+
+export default LengthChecker
