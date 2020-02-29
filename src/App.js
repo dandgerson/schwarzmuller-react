5af343a49dd60ddc83035165cb4949cd74c09ae4
Dmitry G. Anderson <dandgerson@gmail.com>
2020-02-29 18:55:05 +0300

    assignment-2 step 1

--- src/App.js
+++ src/App.js
@@ -2,68 +2,18 @@ import React, { useState } from 'react';
 
 import './App.css';
 
-import Person from './Person'
-
 const App = (props) => {
-  const [persons, setPersons] = useState([
-    { id: 'aselk3', name: 'Dmitry', age: 32 },
-    { id: 'aknee32', name: 'Max', age: 24 },
-    { id: 'fngrrk321', name: 'Stepan', age: 32 },
-  ])
-  const [isPersonsShown, setPersonsShown] = useState(false)
+  const [str, setStr] = useState('')
 
-  const handleDeletePerson = (id) => {
-    setPersons(persons.filter(person => !(person.id === id)))
+  const handleChangeStr = (event) => {
+    setStr(event.target.value)
   }
-
-  const handleChangeName = (event, id) => {
-    setPersons(persons.reduce((acc, current) => {
-      current.id === id ? acc.push({
-        ...current,
-        name: event.target.value,
-      }) : acc.push(current)
-
-      return acc
-    }, []))
-  }
-
-  const handleTogglePersons = () => {
-    setPersonsShown(!isPersonsShown)
-  }
-
-  const style = {
-    backgroundColor: 'white',
-    font: 'inherit',
-    border: '1px solid blue',
-    padding: '8px',
-    cursor: 'pointer',
-  }
-
-  let renderedPersons = null
-  isPersonsShown && (renderedPersons = (
-    <div>
-      {persons.map((person, i) => (
-        <Person
-          key={person.id}
-          handleClick={() => handleDeletePerson(person.id)}
-          handleChange={(event) => handleChangeName(event, person.id)}
-          name={person.name}
-          age={person.age}
-        />
-      ))}
-    </div>
-  ))
 
   return (
     <div className="App">
       <h1>Hi, I'am react App</h1>
-      <button
-        style={style}
-        onClick={handleTogglePersons}
-      >
-        Toggle persons
-      </button>
-      {renderedPersons}
+      <input type="text" onChange={handleChangeStr} value={str} />
+      <p>{str.length}</p>
     </div>
   )
 }
