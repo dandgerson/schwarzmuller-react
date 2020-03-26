0b30825df54f22aba1d3f26c63b56b30fc93f8f9
Dmitry G. Anderson <dandgerson@gmail.com>
2020-03-27 00:53:35 +0300

    102. Rendering adjacent jsx elements   - rendering width hoc-wrapper component

--- src/components/Persons/Person/index.js
+++ src/components/Persons/Person/index.js
@@ -1,27 +1,27 @@
 import React, { Component } from 'react'
 
 import style from './Person.css'
-
+import Aux from 'src/hoc/Aux'
 class Person extends Component {
   render() {
     console.log('[Person.js] rendering...')
-    return ([
-        <p key='i1'>I'm a Person!</p>,
+    return (
+      <Aux>
+        <p>I'm a Person!</p>
         <p
-          key='i2'
           className={style.delete}
           onClick={this.props.handleClick}
         >
           My name is {this.props.name} and I am {this.props.age} years old.
-        </p>,
+        </p>
         <input
-          key='i3'
           type="text"
           onChange={this.props.handleChange}
           value={this.props.name}
-        />,
-        this.props.children && this.props.children,
-    ])
+        />
+        {this.props.children && this.props.children}
+      </Aux>
+    )
   }
 }
 export default Person
